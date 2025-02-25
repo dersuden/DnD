845301398b5a8aa62b2c9096edb4269c4ff340c2
Arthur Valiev <xylophone909@gmail.com>
2025-02-25 19:45:40 +0300

    Main класс не работает как нужно, завтра надо уделить ему внимание и разобраться.

--- src/main/java/org/example/dnd/DnDApplication.java
+++ src/main/java/org/example/dnd/DnDApplication.java
@@ -3,30 +3,47 @@ package org.example.dnd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 @SpringBootApplication
 public class DnDApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DnDApplication.class, args);
-        Hero hero = new Hero();
-        Goblin goblin = new Goblin();
-        GoblinOnWarg goblinOnWarg = new GoblinOnWarg();
-        Orc orc = new Orc();
-//        Glaive glaive = new Glaive(Rarity.RARE);
-//        DualDaggers dualDaggers = new DualDaggers(Rarity.RARE);
-//        GreatSword greatSword = new GreatSword(Rarity.RARE);
-//        System.out.println("Уровень здоровья гоблина: " + goblin.getBaseHp());
-//        System.out.println("Вы наносите удар");
-//        System.out.println("Вы нанесли глефой: " + glaive.hit() + " урона");
-//        System.out.println("Вы нанесли парными клинками: " + dualDaggers.hit() + " урона");
-//        System.out.println("Вы нанесли огромным мечом: " + greatSword.hit() + " урона");
-//        hero.walk(Enemies.GOBLIN);
-        Battle battleWithGoblin = new Battle(hero, goblin);
-        Battle battleWithGoblinOnWarg = new Battle(hero, goblinOnWarg);
-        Battle battleWithOrc = new Battle(hero, orc);
-        battleWithGoblin.heroAttackGoblin();
-        battleWithGoblinOnWarg.heroAttackGoblinOnWarg();
-        battleWithOrc.heroAttackOrc();
-
+//        Hero hero = new Hero();
+//        Goblin goblin = new Goblin();
+//        GoblinOnWarg goblinOnWarg = new GoblinOnWarg();
+//        Orc orc = new Orc();
+//        Battle battleWithGoblin = new Battle(hero, goblin);
+//        Battle battleWithGoblinOnWarg = new Battle(hero, goblinOnWarg);
+//        Battle battleWithOrc = new Battle(hero, orc);
+//        battleWithGoblin.heroAttackGoblin();
+//        battleWithGoblinOnWarg.heroAttackGoblinOnWarg();
+//        battleWithOrc.heroAttackOrc();
+        Map<String, WeaponAttack> weapons = new HashMap<>();
+        weapons.put("Кинжалы", new WeaponAttack("Dual daggers", "D4") {
+        });
+        weapons.put("Лук", new WeaponAttack("Bow", "D6") {
+        });
+        weapons.put("Топор", new WeaponAttack("Axe", "D8") {
+        });
+        weapons.put("Двуручный меч", new WeaponAttack("Great sword", "D10") {
+        });
+        weapons.put("Глефа", new WeaponAttack("Glaive", "D20") {
+        });
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Доступное оружие: Dual daggers, Bow, Axe, Great sword, Glaive");
+        System.out.println("ведите название оружия для атаки: ");
+        String weaponChoice = scanner.nextLine();
+        if (weapons.containsKey(weaponChoice)) {
+            WeaponAttack weaponAttack = weapons.get(weaponChoice);
+            int damage = weaponAttack.rollDamage();
+            System.out.println("Вы атаковали " + weaponAttack.getName() + " и нанесли " + damage + " урона"); {
+            }
+        } else {
+            System.out.println("Оружие не найдено");
+        }
     }
 }
--- src/main/java/org/example/dnd/WeaponAttack.java
+++ src/main/java/org/example/dnd/WeaponAttack.java
@@ -1,24 +1,25 @@
-//package org.example.dnd;
-//
-//import lombok.Data;
-//
-//@Data
-//
-//public abstract class WeaponAttack {
-//    protected Rarity rarity;
-//
-//    public WeaponAttack(Rarity rarity) {
-//        this.rarity = rarity;
-//    }
-//
-//    public int hit() {
-//        int baseDamage = 6;
-//        if (rarity == Rarity.RARE) {
-//            return baseDamage + 3;
-//        }
-//        if (rarity == Rarity.LEGENDARY) {
-//            return baseDamage + 4;
-//        }
-//        return baseDamage;
-//    }
-//}
+package org.example.dnd;
+
+import lombok.Data;
+
+@Data
+public abstract class WeaponAttack {
+    private String name;
+    private String diceType;
+
+    public WeaponAttack(String name, String diceType) {
+        this.name = name;
+        this.diceType = diceType;
+    }
+
+    public int rollDamage() {
+        return switch (diceType) {
+            case "D4" -> D4.rollD4();
+            case "D6" -> D6.rollD6();
+            case "D8" -> D8.rollD8();
+            case "D10" -> D10.rollD10();
+            case "D20" -> D20.rollD20();
+            default -> throw new IllegalArgumentException("Неизвестный тип кубика" + diceType);
+        };
+    }
+}
